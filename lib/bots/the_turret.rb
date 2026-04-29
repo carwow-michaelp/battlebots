dd42dd8ece85a0b7950c9b4b91e203c0a1d62b4a
David Workman <david.workman@carwow.co.uk>
2026-04-29 13:57:26 +0100

    Adding inititals

--- lib/bots/the_turret.rb
+++ lib/bots/the_turret.rb
@@ -3,7 +3,7 @@ require 'bots/bot'
 # DeathRoomba by Elliott
 class TheTurret < BattleBots::Bots::Bot
   def initialize
-    @name = 'The Turret'
+    @name = 'DW: The Turret'
     @speed = 0
     @strength = 25
     @stamina = 25
